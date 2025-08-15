8c60aaf0e4959db8508b0eb76125ca9b1460b4f7
João Mourato <joaomouratocn@gmail.com>
2025-08-14 23:35:16 -0300

    Criada página cadastro de pacientes

---
+++ src/app/register/page.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/button";
+import { Container } from "@/components/container";
+import { Input } from "@/components/input";
+
+export default function () {
+    return (
+        <Container>
+            <div className="flex flex-col gap-3 items-center mt-6">
+                <h1 className="text-4xl font-bold text-white">Cadastrar Pacientes</h1>
+                <div className="flex gap-3 flex-wrap justify-center w-full max-w-[400px]">
+                    <Input
+                        label="Nome"
+                        type="text"
+                        placeholder="Insira o nome do paciente"
+                    />
+
+                    <Input
+                        label="CPF"
+                        type="text"
+                        placeholder="Insira o CPF do paciente"
+                    />
+
+                    <Input
+                        label="Data de nascimento"
+                        type="text"
+                        placeholder="Insira a data de nascimento do paciente"
+                    />
+
+                    <Input
+                        label="Nome da mãe"
+                        type="text"
+                        placeholder="Insira o nome da mãe do paciente"
+                    />
+
+                    <Input
+                        label="Numero do cartão do SUS"
+                        type="text"
+                        placeholder="Insira o numero do cartão do SUS do paciente"
+                    />
+                </div>
+                <Button text="Cadastrar" className="mt-6" />
+            </div>
+        </Container>
+    )
+}
---
+++ src/components/input/index.tsx
@@ -0,0 +1,17 @@
+interface receivedProps {
+    label: string
+    placeholder: string
+    type: string
+}
+export function Input(props: receivedProps) {
+    return (
+        <div className="flex flex-col w-full max-w-[380px]">
+            <label className="pl-2 text-white font-medium">{`${props.label}:`}</label>
+            <input
+                type={props.type}
+                placeholder={props.placeholder}
+                className="bg-[var(--bg-inputs)] px-2 py-1 rounded"
+            />
+        </div>
+    )
+}
